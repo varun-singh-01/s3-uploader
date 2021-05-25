79b640ab19d8fb450a86326249ba16dab203b92f
Varun Singh <varun@atliq.com>
2021-05-25 16:06:18 +0530

    Configured pyCharm for this project

--- getSignedUrl.py
+++ getSignedUrl.py
@@ -6,20 +6,36 @@ import boto3
 
 def get_signed_upload_url(event, context):
 
+    # Parse event body to json
+    event_body = json.loads(event["body"])
+
+    # Create Boto3 client to connect with s3 services
     client = boto3.client("s3")
-    expiration = event["Expiration"] if event.get("Expiration") else 3600
+
+    expiration = event_body["Expiration"] if event_body.get(
+        "Expiration") else 3600
+
+    # Response object
+    res = {
+        "upload_url": None,
+        "FileName": None,
+        "ExpiresIn": expiration
+    }
+
+    # Generate Presigned url for Put operation
     try:
-        url = client.generate_presigned_url('put_object',
-                                            Params={"Bucket": event.get("BucketName"),
-                                                    "Key": event.get("FileName")},
-                                            ExpiresIn=expiration
-                                            )
+        res["upload_url"] = client.generate_presigned_url(ClientMethod="put_object",
+                                                          Params={"Bucket": event_body.get("BucketName"),
+                                                                  "Key": event_body.get("FileName")},
+                                                          ExpiresIn=expiration,
+                                                          HttpMethod="PUT"
+                                                          )
     except Exception as e:
         print("[getSignedUrl] Error :: {}".format(e))
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(url)
+        "body": json.dumps({"url": res})
     }
 
     return response
